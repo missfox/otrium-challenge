72764f685d2d4388f581b8feb49f08637eb9e126
Olesya Nefedova <missfox115@gmail.com>
2022-02-16 19:05:47 +0300

    task: Added Facet and FacetItems components

---
+++ src/components/Facet/Facet.tsx
@@ -0,0 +1,73 @@
+import cloneDeep from 'lodash/cloneDeep';
+import React from 'react';
+import { FacetDictionary, FacetItem } from '../../interfaces/facet';
+import { rootNodeId } from './constants';
+import FacetItems from './FacetItems';
+
+interface FacetParams {
+  items: FacetDictionary;
+  onItemsChange: (updatedItems: FacetDictionary) => void;
+}
+
+function Facet(props: FacetParams) {
+  const { items, onItemsChange } = props;
+
+  // Recursive function to traverse a dictionary and get all children for a given parent node
+  const getAllChildren = (parent: FacetItem, childrenIds: string[]): string[] => {
+    // If node has no children, then exit
+    if (parent.children.length === 0) {
+      return childrenIds;
+    }
+    // Else check all children
+    parent.children.forEach((childId: string) => {
+      // Add child id to the result array
+      childrenIds.push(childId);
+      // Call the function again and get all children for given children element
+      getAllChildren(items.get(childId) as FacetItem, childrenIds);
+    });
+
+    return childrenIds;
+  };
+
+  const handleCheck = (itemId: string, checked: boolean): void => {
+    // Get an item by id and change its checked value with the received checked parameter
+    const item = items.get(itemId) as FacetItem;
+    item.checked = checked;
+    // Get all children from checked parent node
+    const checkedChildren: string[] = getAllChildren(item, []);
+
+    // Make all children checked
+    checkedChildren.forEach((childId: string) => {
+      const child = items.get(childId) as FacetItem;
+      child.checked = checked;
+    });
+    // Clone options and update state
+    const clonedOptions = cloneDeep(items);
+    onItemsChange(clonedOptions);
+  };
+
+  const handleClick = (itemId: string, expanded: boolean, checked: boolean): void => {
+    // Get an item by id and change its expanded value with the received expanded parameter
+    const item = items.get(itemId) as FacetItem;
+    item.expanded = expanded;
+    item.checked = checked;
+    // Clone options and update state
+    const clonedOptions = cloneDeep(items);
+    onItemsChange(clonedOptions);
+  };
+
+  return items.has(rootNodeId) ? (
+    <div>
+      <FacetItems
+        childItems={(items.get(rootNodeId) as FacetItem).children}
+        checked={false}
+        expanded={false}
+        allItems={items}
+        onCheck={handleCheck}
+        onClick={handleClick}
+      />
+    </div>
+  ) : null;
+}
+
+export default Facet;
---
+++ src/components/Facet/FacetItems.tsx
@@ -0,0 +1,77 @@
+import React, { useMemo } from 'react';
+import { FacetItem } from '../../interfaces/facet';
+import FacetButton from './FacetButton';
+
+interface FacetItemsParams {
+  id?: string,
+  name?: string,
+  checked: boolean,
+  expanded: boolean,
+  childItems: string[],
+  allItems: Map<string, FacetItem>,
+  onCheck: (id: string, checked: boolean) => void,
+  onClick: (id: string, expanded: boolean, checked: boolean) => void,
+}
+
+// Recursively render facet items
+function FacetItems(props: FacetItemsParams) {
+  const {
+    id,
+    name,
+    checked,
+    expanded,
+    childItems,
+    allItems,
+    onCheck,
+    onClick,
+  } = props;
+
+  // If an item doesn't have id and name then it's the first level item
+  const isFirstLevel = useMemo(() => !id && !name, [id, name]);
+
+  return (
+    <>
+      {!isFirstLevel
+        ? (
+          <FacetButton
+            onClick={onClick}
+            id={id as string}
+            name={name as string}
+            onCheck={onCheck}
+            checked={checked}
+            expanded={expanded}
+          >
+            {name}
+          </FacetButton>
+        )
+        : null}
+      {childItems.map((childId) => {
+        const {
+          name: childName = '', children: innerChildItems, checked: childChecked, expanded: childExpanded,
+        } = allItems.get(childId) as FacetItem;
+
+        return (
+          <div key={childId} className={`facet-items ${(expanded || isFirstLevel) ? 'facet-items_expanded' : ''}`}>
+            <FacetItems
+              id={childId}
+              name={childName}
+              checked={childChecked}
+              expanded={childExpanded}
+              allItems={allItems}
+              childItems={innerChildItems}
+              onCheck={onCheck}
+              onClick={onClick}
+            />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+FacetItems.defaultProps = {
+  id: '',
+  name: '',
+};
+
+export default FacetItems;
